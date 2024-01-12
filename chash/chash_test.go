925ba9cf96a25b5ce0631a2b35d04c156a526bd8
weizengyipp <weizengyipp@gmail.com>
2024-01-12 14:33:14 +0800

    consistence cache and tests

---
+++ chash/chash_test.go
@@ -0,0 +1,45 @@
+package chash
+
+import (
+	"strconv"
+	"testing"
+)
+
+var hashFunc = func(data []byte) uint32 {
+	i, _ := strconv.Atoi(string(data))
+	return uint32(i)
+}
+
+func TestHashing(t *testing.T) {
+	hash := New(3, hashFunc)
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("6", "4", "2")
+	t.Log(hash)
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// Adds 8, 18, 28
+	hash.Add("8")
+	t.Log(hash)
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+}
